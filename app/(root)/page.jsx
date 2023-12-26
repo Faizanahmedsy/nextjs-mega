b20aada468c921e2b0f5da4de3ebef04289ac814
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-26 09:36:11 +0530

    foo2

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import { assets } from "@/lib/utils/asset-utils";
 import Image from "next/image";
 import React from "react";
 
+// opaity-70
+// opaity-55
+
 const Home = () => {
+  const [showBackground, setShowBackground] = React.useState(false);
+
+  React.useEffect(() => {
+    // setTimeout(() => {
+    //   setShowBackground(true);
+    // }, 1000);
+    setShowBackground(true);
+  }, []);
+
   return (
     <main>
+      <div className="fixed inset-0 bg-blue-200 transition-colors delay-100 duration-700 opacity-25 "></div>
       <Image
         width={1200}
         height={1200}
@@ -14,12 +30,19 @@ const Home = () => {
         src={assets.gradient}
       />
       <div
-        className="fixed inset-0 opacity-30"
+        className="fixed inset-0 opacity-55"
         style={{
           backgroundImage: `url(${assets.square})`,
           backgroundSize: "30px",
         }}
       />
+
+      <div
+        className={cn(
+          "bg-black fixed inset-0 transition-opacity duration-[1500ms]",
+          !showBackground ? "opacity-100" : "opacity-0"
+        )}
+      ></div>
     </main>
   );
 };
